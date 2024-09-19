5193f3bff0fc355c6ab33da48c22a9677226ca66
mel418 <melodygatan@gmail.com>
2024-09-19 10:14:50 -0700

    finished results/stats and screenshots

--- OneCardWar/war.cpp
+++ OneCardWar/war.cpp
@@ -32,6 +32,8 @@ int main() {
     cout << "\n Shuffled Deck:" << endl;
     myDeck.print();
 
+    int results[3] = {0, 0, 0};
+
     for (int i = 0; i < 26; i++) {
         Card card1 = myDeck.deal();
         Card card2 = myDeck.deal();
@@ -45,15 +47,25 @@ int main() {
         int result = card1.compare(card2);
         if (result == 1) {
             cout<< player1<< "=> Winner"<<endl;
+            results[0] += 1;
         }
         else if (result == -1)
         {
             cout<< player2<< "=> Winner"<<endl;
+            results[1]+= 1;
         }
         else {
             cout<< "Tie game"<<endl;
+            results[2] += 1;
         } 
     } 
+
+    cout<< "\n------Final Stats-------";
+    cout<<"\n\t" << player1 << "\tvs.\t" << player2<< endl;
+    cout<< "Wins \t" << results[0] << "\t\t" << results[1]<< endl;
+    cout<< "Losses \t" << 26 - (results[0]+ results[2]) << "\t\t" << 26 - (results[1]+ results[2]) << endl;
+    cout<< "Ties \t" << results[2] << "\t\t" << results[2] << endl;
+    
 
     return 0;
 }
